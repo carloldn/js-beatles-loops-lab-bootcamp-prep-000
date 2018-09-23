6d41e9bbdb371415bf1c6f3b6ef05e1c17b7a62a
carlo montanaro <carloldn@outlook.com>
2018-09-23 22:15:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
-    var allPlayers = [];
-    for (var i=0; i < musiciansplayers.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
+    var allMusicians = [];
+    for (var i=0; i < musicians.length; i++) {
+        allPlayers.push(musicians[i] + " plays " + instruments[i]);
     }
-    return allPlayers;
+    return allMusicians;
 }
